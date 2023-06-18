942d64c78af6b7672cef8863d92c93e75f8c5ba7
SoDeRMond <SoDeRMond@gmail.com>
2023-06-18 13:04:49 +0300

    Corrected Version

--- ILMergeUI/Properties/AssemblyInfo.cs
+++ ILMergeUI/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("414fc4dd-acae-411b-bf38-9476168b210f")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
